d0e5ef03d83503e69932db9112207b15be0c46ac
robstowe <robstowe94@gmail.com>
2023-05-25 14:57:54 -0700

    added the function to save on page / fixed hour 9

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var currentTime = $("#currentTime");
 var isPresent = dayjs().format("H");
 var saveBtn = $("#save").text();
 //function - updates the time to work on page, brings up the date format via dayjs.
-$(function() {
+$(function () {
     function updateTime() {
         var isPresent = dayjs();
         currentTime.text(isPresent.format("MMM D, YYYY h:mm:ss a"));
@@ -13,32 +13,20 @@ $(function() {
     setInterval(updateTime, 1000)
 })
 
-//work our variables to define each hour
-var workHours = [
-    (hour9El = $("#hour-9")),
-    (hour10El = $("#hour-10")),
-    (hour11El = $("#hour-11")),
-    (hour12El = $("#hour-12")),
-    (hour13El = $("#hour-13")),
-    (hour14El = $("#hour-14")),
-    (hour15El = $("#hour-15")),
-    (hour16El = $("#hour-16")),
-    (hour17El = $("#hour-17")),
-];
 //save button function to work on click
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
     var textValue = $(this).siblings("textarea").val();
     localStorage.setItem(timeBlockId, textValue);
-});    
+});
 
 
-  
+
 // function to loop through each time block, and adds classes to define past present and future
-$(".time-block").each(function() {
+$(".time-block").each(function () {
     var timeBlock = $(this).attr("id");
     var timeBlockHour = timeBlock.split("-")[1];
-    
+
     if (timeBlockHour < isPresent) {
         $(this).removeClass("present future").addClass("past");
     } else if (timeBlockHour === isPresent) {
@@ -48,3 +36,13 @@ $(".time-block").each(function() {
     }
 });
 
+
+
+$('.time-block').each(function () {
+    var timeBlock = $(this).attr('id');
+    var meetings = localStorage.getItem(timeBlock);
+
+    if (meetings) {
+        $(this).find("textarea").val(meetings);
+    }
+});
